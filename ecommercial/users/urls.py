a048e2e210473eaaee1d3edf369ccc898d6ce9bd
comrider <cena3799@gmail.com>
2022-09-04 17:09:48 +0530

    reference templates are added

--- ecommercial/users/urls.py
+++ ecommercial/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from . import views 
+from . import views
 urlpatterns = [
     path('',views.home, name='user_home'),
-    path('shop/',views.shop, name='home_shop'),
+    path('shop/',views.shop, name='user_shop'),
     path('contact/',views.contact, name='user_contact'),
     path('about/',views.about, name='user_about'),
-    # path('',views.home, name='home'),
-    
+    path('forget-password/',views.forget_password, name='user_forget_password'),
+    path('login/',views.login, name='user_login'),
+    path('register/',views.register, name='user_register'),
 ]
--- ecommercial/users/views.py
+++ ecommercial/users/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html',{})
+    return render(request, 'user/index.html',{})
 def shop(request):
-    return render(request, 'shop.html',{})
+    return render(request, 'user/shop.html',{})
 def contact(request):
-    return render(request, 'contact.html',{})
+    return render(request, 'user/contact.html',{})
 def about(request):
-    return render(request, 'about.html',{})
-# def home(request):
-#     return render(request, 'about.html',{})
+    return render(request, 'user/about.html',{})
+
+def forget_password(request):
+     return render(request, 'sneat/auth-forgot-password-basic.html',{})
+def login(request):
+     return render(request, 'sneat/auth-login-basic.html',{})
+def register(request):
+     return render(request, 'sneat/auth-register-basic.html',{})
